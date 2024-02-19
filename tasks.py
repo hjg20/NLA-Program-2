7b5e88297ba942b54c0c8a7ea30ce0a163e1987f
hjg20 <huntergarrison2020@gmail.com>
2024-02-19 15:38:22 -0500

    u

--- tasks.py
+++ tasks.py
@@ -1,19 +1,30 @@
-import utils as u
+# import utils as u
 import numpy as np
 
 
-A = [
-    [2,0,0,0,0],
-    [3,2,0,0,0],
-    [4,3,2,0,0],
-    [5,4,0,2,0],
-    [6,5,4,3,2]
-]
+A = np.array([
+    [0,0,0,0,1],
+    [0,0,0,2,0],
+    [0,0,3,0,0],
+    [0,4,0,0,0],
+    [5,0,0,0,0]
+])
 
-P, Q, L, U = u.lu(A, "partial pivoting")
+# P, Q, L, U = u.lu(A, "no pivoting")
 
-print('L:', np.array(L),'\n\n', 'U:', np.array(U), '\n\n', 'P_r:', np.array(P), '\n\n', 'P_c:', np.array(Q))
+# print('L:', np.array(L),'\n\n', 'U:', np.array(U), '\n\n', 'P_r:', np.array(P), '\n\n', 'P_c:', np.array(Q))
 
+def LUdecomp(a):
+    n = len(a)
+    for k in range(0,n-1):
+        for i in range(k+1,n):
+            if a[i,k] != 0.0:
+                lam = a [i,k]/a[k,k]
+                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+                a[i,k] = lam
+    return a
+
+print(LUdecomp(A))
 
 
 
